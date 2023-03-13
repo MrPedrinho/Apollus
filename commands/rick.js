18c81a15309cb929cec43671d1b4972f295b9407
pedrolopes <pedromslopes22@gmail.com>
2023-03-13 19:28:35 +0000

    Updated rick roll order, image comes first now

--- commands/rick.js
+++ commands/rick.js
@@ -22,8 +22,16 @@ module.exports = {
         if (message.guild.members.me.voice.channel) {
             if (vc !== message.guild.members.me.voice.channel) return message.reply(lang === "pt" ? "Tens de estar no mesmo voice chat" : "You need to be in the same voice chat")
         }
-        await guild.setConnection(message, vc)
 
+        try {
+            message.channel.send({
+                files: ["./rickroll-image.jpg"]
+            })
+        } catch {
+            message.channel.send(`Never gonna give you up <@!${message.author.id}>`)
+        }
+
+        await guild.setConnection(message, vc)
 
         const {video_details} = await video_info("https://www.youtube.com/watch?v=dQw4w9WgXcQ",)
 
@@ -35,15 +43,6 @@ module.exports = {
             author: message.author,
             channel: message.channel,
         }
-
         await guild.playNow(song)
-
-        try {
-            message.channel.send({
-                files: ["./rickroll-image.jpg"]
-            })
-        } catch {
-            message.channel.send(`Never gonna give you up <@!${message.author.id}>`)
-        }
     }
 }
